6b288ab36f9efe9ce39e463aa17fedf9290fb107
GabrielRf <ti.gabrielrf@gmail.com>
2022-12-30 17:15:43 -0300

    Feat: Server-side rendering of posts, and static pages for each posts

--- sanity-yt-build/next.config.js
+++ sanity-yt-build/next.config.js
@@ -2,14 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "links.papareact.com",
-        port: "",
-        pathname: "/yvf"
-      }
-    ]
+    domains: ['cdn.sanity.io'],
   }
 }
 
